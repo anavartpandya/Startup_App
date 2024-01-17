8670351a79992fc2326dca4fffa2fac0941126bf
anavartpandya <76461711+anavartpandya@users.noreply.github.com>
2024-01-18 04:13:02 +0530

    Update app.py

--- FlashcardBackend/app.py
+++ FlashcardBackend/app.py
@@ -5,9 +5,14 @@ app = Flask(__name__)
 @app.route('/modify_flashcard', methods=['POST'])
 def modify_flashcard():
     data = request.json
-    front_text = data['frontText'] + " got it"
-    back_text = data['backText'] + " got it"
+    front_text = data['frontText'] + " got its"
+    back_text = data['backText'] + " got its"
     return jsonify({'frontText': front_text, 'backText': back_text})
+
+@app.route('/health-check')
+def health_check():
+    print('ok')
+    return jsonify({'status': 'ok'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
